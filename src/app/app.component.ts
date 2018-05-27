02e98c1fc4eaa6cdce9802a7d40b9e84715e90c1
BaaniLeen <baani.jolly@gmail.com>
2018-05-27 19:08:03 +0530

    Added code

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  constructor(){
+    this.onStartup();
+  }
+
+onStartup(){
+  console.log('Hello TS World');
+  var header = document.getElementById("myDIV");
+  var btns = header.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+}
+}
+
